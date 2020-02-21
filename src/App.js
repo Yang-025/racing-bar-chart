0cb3fe3a874935dbb21978a145577e0417b5ae4c
Yang025 <kkadso@gmail.com>
2020-02-21 15:32:41 +0800

    d3js和reactjs的合作

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import * as d3 from "d3";
+import "./App.css";
 
 function App() {
+  const svgRef = useRef();
+  const data = [10, 20, 30, 40, 50];
+  const width = 800;
+  const height = 650;
+  useEffect(() => {
+    const svg = d3.select(svgRef.current);
+
+    svg.style("width", width).style("height", height);
+
+    svg
+      .selectAll("rect")
+      .data(data, d => d)
+      .join("rect")
+      .attr("x", 0)
+      .attr("y", (d, i) => i * 50)
+      .attr("width", d => d)
+      .attr("height", 50)
+      .attr("fill", "DarkBlue");
+  }, [data, svgRef.current]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div>
+        <h3>{`svg ${width}* ${height}`}</h3>
+        <svg ref={svgRef} />
+      </div>
     </div>
   );
 }
